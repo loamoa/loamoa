fc8a968e1dc8d4abc46d9ef6bead712a997f8b38
qsc7342 <qsc7342@hanyang.ac.kr>
2022-01-24 11:22:25 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/loamoa/loamoa/LoamoaApplication.java
@@ -0,0 +1,13 @@
+package com.loamoa.loamoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoamoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoamoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loamoa/loamoa/LoamoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loamoa.loamoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoamoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
