7eabb3797436cd5f8661193ff70790bbeda76413
kimseunghyun <seunghyun4525@gmail.com>
2022-01-28 09:56:51 +0900

    "feat: 비즈니스 로직 설계 findItems() : DB에 저장된 모든 아이템 가격 정보를 가져온다. refreshItems() : DB에 저장된 아이템명에 대해서 가격 정보를 업데이트한다. "

--- src/main/java/com/loamoa/loamoa/service/ItemService.java
+++ src/main/java/com/loamoa/loamoa/service/ItemService.java
@@ -1,19 +1,67 @@
 package com.loamoa.loamoa.service;
 
-import com.loamoa.loamoa.repository.ItemRepository;
+import com.loamoa.loamoa.domain.Item;
+import com.loamoa.loamoa.domain.MarketForm;
+import com.loamoa.loamoa.repository.MemoryItemRepository;
+import com.loamoa.loamoa.selenium.TaskSelenium;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemService {
-    private final ItemRepository itemRepository;
+    private final MemoryItemRepository itemRepository;
+    private final TaskSelenium taskSelenium;
 
     @Autowired
-    public ItemService(ItemRepository itemRepository) {
+    public ItemService(MemoryItemRepository itemRepository, TaskSelenium taskSelenium) {
         this.itemRepository = itemRepository;
+        this.taskSelenium = taskSelenium;
     }
 
-    public void test() {
-        itemRepository.test();
+    /**
+     * 전체 아이템 가격 조회
+     * @return 조회한 아이템 리스트
+     */
+    public List<Item> findItems() {
+        List<Item> itemList = itemRepository.findAll();
+        for(Item i : itemList) {
+            System.out.println("[ItemService] "+i.toString());
+        }
+        return itemList;
+    }
+
+    public void refreshItems() {
+        try {
+            int type;
+            int MAX_TYPE = 2;
+            for (type = 0; type < MAX_TYPE; type++) {
+                // 저장소에서 type별로 검색할 아이템명을 모두 가져온다.
+                List<String> itemNames = itemRepository.findAllItemName(type);
+                System.out.println(type + itemNames.toString());
+                // 각각의 아이템명에 따라서 검색 결과를 업데이트한다.
+                for (String name : itemNames) {
+                    // 여기는 검색별로 상수 설정하는건데 나중에 메소드로 빼거나 하면 될듯?
+                    MarketForm form = new MarketForm();
+                    if (type == 0) {
+                        // 각인서 폼으로 설정
+                        form.setFirstCategory(40000L); // 각인서
+                        form.setSecondCategory(0L); // 각인서-전체
+                        form.setGrade(4L); // 전설 등급
+                    } else if (type == 1) {
+                        // 재료 폼으로 설정
+                        form.setFirstCategory(50000L); // 강화 재료
+                        form.setSecondCategory(0L); // 강화 재료-전체
+                        form.setTier(3L); // 3티어 강화재료
+                    }
+                    form.setItemName(name);
+                    itemRepository.save(taskSelenium.getItem(form).get());
+                }
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }
