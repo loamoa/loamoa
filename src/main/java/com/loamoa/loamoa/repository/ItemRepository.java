d41f5378d32e6bea2db84c9ccc9318524008d3ef
kimseunghyun <seunghyun4525@gmail.com>
2022-01-28 09:50:54 +0900

    "feat: ItemRepository를 interface로 변경하여 추후 DB 연동의 편의성 제공 "

--- src/main/java/com/loamoa/loamoa/repository/ItemRepository.java
+++ src/main/java/com/loamoa/loamoa/repository/ItemRepository.java
@@ -1,35 +1,13 @@
 package com.loamoa.loamoa.repository;
 
 import com.loamoa.loamoa.domain.Item;
-import com.loamoa.loamoa.selenium.TaskSelenium;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class ItemRepository {
+import java.util.List;
+import java.util.Optional;
 
-    private final TaskSelenium taskSelenium;
-
-    @Autowired
-    public ItemRepository(TaskSelenium taskSelenium) {
-        this.taskSelenium = taskSelenium;
-    }
-
-    /**
-     * 각인서 테스트
-     */
-    public void test() {
-        String booknames[] = {"원한 각인서", "저주받은 인형 각인서", "예리한 둔기 각인서"};
-        for(String s : booknames) {
-            Item item = taskSelenium.getBookItemByName(s).get();
-            System.out.println(item.toString());
-        }
-    }
-
-    /**
-     * 셀레니움의 크롬 드라이버를 중단시킨다.
-     */
-    public void cleanDriver() {
-        taskSelenium.stopSelenium();
-    }
+public interface ItemRepository {
+    Item save(Item item);
+    List<Item> findAll();
+    Optional<Item> findByName(String itemName);
+    List<String> findAllItemName(int type);
 }
